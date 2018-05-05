f951d6481257548c0e32e4744940ed801967e468
Akhil Jalan <akhiljalan@berkeley.edu>
2018-05-05 16:17:50 -0700

    update filepaths and add image stylization from magenta

---
+++ true_gan.py
@@ -0,0 +1,136 @@
+import sys
+import os
+import numpy as np
+import tensorflow as tf
+
+sys.path.append('../arbitrary_image_stylization(modified)/')
+sys.path.append('../discriminator/')
+sys.path.append('../image_stylization')
+import arbitrary_image_stylization_build_model as build_model
+from data_processing_utils import *
+
+def discriminator_network(images, reuse_val=tf.AUTO_REUSE, is_training=True): 
+	with tf.variable_scope(tf.get_variable_scope(), reuse=reuse_val) and tf.name_scope('discriminator'):
+		conv1_out = slim.conv2d(images, 1, [5, 5], activation_fn=tf.nn.relu, scope='discriminator/conv1')
+		pool1_out = slim.max_pool2d(conv1_out, [2, 2], stride=2, scope='discriminator/pool1')
+		conv2_out = slim.conv2d(pool1_out, 1, [4, 4], stride=2, activation_fn=tf.nn.relu, scope='discriminator/conv2')
+		pool2_out = slim.max_pool2d(conv2_out, [6, 6], stride=3, padding='SAME', scope='discriminator/pool2')
+		flat_out = slim.flatten(pool2_out, scope='discriminator/flatten3')
+		fc1_out = slim.fully_connected(flat_out, 12, scope='discriminator/fc1', activation_fn=tf.nn.relu)
+		predictions = slim.fully_connected(fc1_out, 2, scope='discriminator/fc2', activation_fn=tf.nn.softmax)
+		return predictions
+
+def main():
+	tf.reset_default_graph()
+	BATCH_SIZE = 1
+	with tf.Graph().as_default(), tf.Session() as sess:
+		# Loads content images.
+
+		#TODO load test images from a different test path. Do a small amount for simplicity
+		# Make sure it's the same content, style images as the test set for the GAN 
+
+		# todo insert content_path, style_path... 
+		content_inputs_ = load_random_images(content_path, batch_size=BATCH_SIZE)
+		
+		# Loads evaluation style images.
+		style_inputs_ = load_random_images(style_path, batch_size=BATCH_SIZE)
+		
+		# Default style, content, and variation weights from magenta. 
+		
+		content_weights = {"vgg_16/conv3": 1}
+		style_weights = {"vgg_16/conv1": 0.5e-3, "vgg_16/conv2": 0.5e-3,
+										 "vgg_16/conv3": 0.5e-3, "vgg_16/conv4": 0.5e-3}
+		total_variation_weight = 1e4
+		
+		stylized_images, total_loss_pass_1, loss_dict_pass_1, _ = build_model.build_model(
+			content_inputs_,
+			style_inputs_,
+			trainable=True,
+			is_training=True,
+			inception_end_point='Mixed_6e',
+			style_prediction_bottleneck=100,
+			adds_losses=True,
+			content_weights=content_weights,
+			style_weights=style_weights,
+			total_variation_weight=total_variation_weight)
+		
+		unstylized_images, total_loss_pass_2, loss_dict_pass_2, _ = build_model.build_model(
+			stylized_images, #stylized as content
+			content_inputs_, #original content as style
+			reuse=True,
+			trainable=True,
+			is_training=True,
+			inception_end_point='Mixed_6e',
+			style_prediction_bottleneck=100,
+			adds_losses=True,
+			content_weights=content_weights,
+			style_weights=style_weights,
+			total_variation_weight=total_variation_weight)
+		
+		# Log all losses to tensorboard. 
+		loss_dict = {}
+		loss_dict.update(loss_dict_pass_1)
+		loss_dict.update(loss_dict_pass_2)
+		for key, value in loss_dict.iteritems():
+			tf.summary.scalar(key, value)
+
+		# Log images to tensorboard 
+		tf.summary.image('image/0_content_inputs', content_inputs_, 3)
+			# tf.summary.image('image/1_style_inputs_orig', style_inputs_orig_, 3)
+		tf.summary.image('image/1_style_inputs', style_inputs_, 3)
+		tf.summary.image('image/2_stylized_images', stylized_images, 3)
+		tf.summary.image('image/3_unstylized_images', unstylized_images, 3)
+				
+				
+
+		discrim_predictions = discriminator_network(content_inputs_)
+
+		# def gen_labels(is_real = True, batch_size = 4): 
+
+		# Generate label tensors on the fly. 
+		discrim_loss = slim.losses.softmax_cross_entropy(discrim_predictions, gen_labels(is_real=False, batch_size=BATCH_SIZE))
+		gen_fooling_loss = slim.losses.softmax_cross_entropy(discrim_predictions, gen_labels(is_real=True, batch_size=BATCH_SIZE))
+		
+				
+		gen_optimizer = tf.train.AdamOptimizer(learning_rate=1e-2)
+		gen_train_op = slim.learning.create_train_op(
+			total_loss_pass_1 + total_variation_weight * gen_fooling_loss, # + total_loss_pass_2? 
+			gen_optimizer,
+			summarize_gradients=True)
+
+		discr_optimizer = tf.train.AdamOptimizer(learning_rate=1e-2)
+		discr_train_op = slim.learning.create_train_op(
+			discrim_loss, # + total_loss_pass_2? 
+			discr_optimizer,
+			summarize_gradients=True)		
+		
+		# todo merge train ops 
+
+		# Get checkpoint files. 
+		# See above for the inception, vgg checkpoints. 
+		
+		# TODO change checkpoint path...
+		gen_checkpoint = '/Users/akhiljalan/Documents/arbitrary_style_transfer/model.ckpt'
+		discrim_checkpoint = './logdir/model.ckpt-85'
+		
+		model_vars = slim.get_variables_to_restore()
+		# No saved model yet! 
+		discrim_var_names = [var for var in model_vars if 'discriminator' in var.name]
+		gen_var_names = [var for var in model_vars if 'discriminator' not in var.name]
+
+		gen_assign_op, gen_feed_dict = slim.assign_from_checkpoint(gen_checkpoint, gen_var_names) #TODO change this...
+		# discrim_assign_op, discrim_feed_dict = slim.assign_from_checkpoint(discrim_checkpoint,
+		#                                            discrim_var_names)
+		
+
+		def init_assign_func(sess):
+			sess.run(gen_assign_op, gen_feed_dict)				
+
+		
+		slim.learning.train(
+			train_op=train_op, #todo replace with merged train op. 
+			logdir='./logdir/',
+			number_of_steps=1,
+			init_fn=init_assign_func,
+			save_summaries_secs=1,
+			save_interval_secs=1)
