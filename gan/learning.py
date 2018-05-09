reordered files and fixed bugs. still getting incorrect results

gan/learning.py
@@ -21,7 +21,7 @@ from __future__ import print_function
 import numpy as np
 import tensorflow as tf
 
-from image_stylization import vgg
+import vgg
 
 slim = tf.contrib.slim
 

gan/model.py
@@ -19,8 +19,7 @@ from __future__ import print_function
 
 
 import tensorflow as tf
-
-from image_stylization import ops
+import ops
 
 slim = tf.contrib.slim
 

gan/nza_model.py
@@ -22,8 +22,7 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
-
-from image_stylization import model as model_util
+import model as model_util
 
 slim = tf.contrib.slim
 